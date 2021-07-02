cd6bf286a007af0012236b1ce21be993dea8870e
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-02 15:39:37 +0530

    data get set

---
+++ app/src/main/java/com/example/android/myapplication/Data.java
@@ -0,0 +1,22 @@
+package com.example.android.myapplication;
+
+public class Data {
+    String name;
+    int image;
+
+
+    public Data(String name , int image) {
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+
+}
