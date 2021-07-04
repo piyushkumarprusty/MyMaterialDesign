a2882de418cb17756cef9a454ba45c057e646be7
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-04 18:06:16 +0530

    openUrlUsingIntent.java D

---
+++ app/src/main/java/com/example/android/myapplication/openUrlUsingIntent.java
@@ -0,0 +1,55 @@
+package com.example.android.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class openUrlUsingIntent extends AppCompatActivity {
+    TextView github;
+    Button unslapsh;
+    ImageView youtube;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_url_using_intent);
+
+        github = findViewById(R.id.github);
+        unslapsh = findViewById(R.id.unsplash);
+        youtube = findViewById(R.id.youtube);
+
+        github.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            gotoUrl("https://github.com/piyushkumarprusty");
+        }
+    });
+
+        unslapsh.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            gotoUrl("https://www.udemy.com/");
+        }
+    });
+
+        youtube.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            gotoUrl("https://www.youtube.com/");
+        }
+    });
+
+
+    }
+
+    private void gotoUrl(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW));
+    }
+}
