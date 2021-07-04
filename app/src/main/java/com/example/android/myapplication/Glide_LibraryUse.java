2fe60857253b717c58238bfdad3424268c51aafe
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-04 17:40:39 +0530

    Glide_LibraryUse.java D

--- app/src/main/java/com/example/android/myapplication/Glide_LibraryUse.java
+++ app/src/main/java/com/example/android/myapplication/Glide_LibraryUse.java
@@ -3,20 +3,21 @@ package com.example.android.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class Glide_LibraryUse extends AppCompatActivity {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glide_library_use);
+        getSupportActionBar().hide();
+        imageView = findViewById(R.id.imageViewg);
 
-
-
-
-
-
-
+        Glide.with(this).load("https://unsplash.com/photos/aKbNRT-PKqQ").placeholder(R.drawable.kk).into(imageView);
 
 
     }
