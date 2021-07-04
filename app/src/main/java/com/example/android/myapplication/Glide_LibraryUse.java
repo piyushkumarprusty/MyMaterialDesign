a653f94e8502f595e1b73978973215f56828a1fa
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-04 17:49:29 +0530

    Picasso library use

--- app/src/main/java/com/example/android/myapplication/Glide_LibraryUse.java
+++ app/src/main/java/com/example/android/myapplication/Glide_LibraryUse.java
@@ -3,22 +3,24 @@ package com.example.android.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
 
 public class Glide_LibraryUse extends AppCompatActivity {
-    ImageView imageView;
-
+    ImageView imageViewg ,  imageViewp  ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glide_library_use);
         getSupportActionBar().hide();
-        imageView = findViewById(R.id.imageViewg);
+        imageViewg = findViewById(R.id.imageViewg);
+        imageViewp = findViewById(R.id.imageViewp);
 
-        Glide.with(this).load("https://unsplash.com/photos/aKbNRT-PKqQ").placeholder(R.drawable.kk).into(imageView);
-
+        Glide.with(this).load("https://unsplash.com/photos/aKbNRT-PKqQ").placeholder(R.drawable.kk).into(imageViewg);
+        Picasso.get().load("https://raw.githubusercontent.com/square/picasso/master/website/static/sample.png").into(imageViewp);
 
     }
 }
