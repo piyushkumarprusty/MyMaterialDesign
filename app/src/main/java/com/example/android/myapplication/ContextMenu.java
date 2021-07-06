5a139a9fc3c07759b469d262f1120f7f9004d7ee
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-07 00:04:03 +0530

    ContextMenu.java || option menu text

--- app/src/main/java/com/example/android/myapplication/ContextMenu.java
+++ app/src/main/java/com/example/android/myapplication/ContextMenu.java
@@ -4,13 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ContextMenu extends AppCompatActivity {
-Button b;
+    Button b;
+    TextView t1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +22,38 @@ Button b;
 
         b = findViewById(R.id.b1);
         registerForContextMenu(b);
+        t1 = findViewById(R.id.t1);
     }
 
+    // option menu text
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemid;
+        itemid = item.getItemId();
+        if (itemid == R.id.cm1)
+            t1.setText("Menu1 is selected");
+        if (itemid == R.id.cm2)
+            t1.setText("Menu2 is selected");
+        if (itemid == R.id.cm3)
+            t1.setText("Menu3 is selected");
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // context menu button
 
     @Override
     public void onCreateContextMenu(android.view.ContextMenu menu, View v, android.view.ContextMenu.ContextMenuInfo menuInfo) {
-        getMenuInflater().inflate(R.menu.menu,menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
 
         super.onCreateContextMenu(menu, v, menuInfo);
     }
@@ -32,13 +62,13 @@ Button b;
     public boolean onContextItemSelected(@NonNull MenuItem item) {
         int itemid;
         itemid = item.getItemId();
-        if (itemid == R.id.cm1){
+        if (itemid == R.id.cm1) {
             Toast.makeText(this, "This is Home menu", Toast.LENGTH_SHORT).show();
         }
-          if (itemid == R.id.cm2){
+        if (itemid == R.id.cm2) {
             Toast.makeText(this, "This is Insert menu", Toast.LENGTH_SHORT).show();
         }
-          if (itemid == R.id.cm3){
+        if (itemid == R.id.cm3) {
             Toast.makeText(this, "This is Update menu", Toast.LENGTH_SHORT).show();
         }
 
