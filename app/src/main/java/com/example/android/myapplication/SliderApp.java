7ab36892b510621c355aeef58a86c27a73c309e6
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-18 20:17:48 +0530

    SliderApp set img

--- app/src/main/java/com/example/android/myapplication/SliderApp.java
+++ app/src/main/java/com/example/android/myapplication/SliderApp.java
@@ -14,18 +14,27 @@ public class SliderApp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slider_app);
-
+        //getActionBar().hide();
         int imgarray[] = {R.drawable.pic17, R.drawable.pic7, R.drawable.pic6};
 
         flipper = findViewById(R.id.flipper);
+        for (int i = 0; i < imgarray.length; i++) {
+            showImage(imgarray[i]);
+        }
 
     }
 
-    public void showImage(int img){
+    public void showImage(int img) {
         ImageView imageView = new ImageView(this);
         imageView.setBackgroundResource(img);
-    }
+        flipper.addView(imageView);
+        flipper.setFlipInterval(2000);
+        flipper.setAutoStart(true);
 
+        flipper.setInAnimation(this, android.R.anim.slide_out_right);
+        flipper.setOutAnimation(this, android.R.anim.slide_in_left);
+
+    }
 
 
 }
