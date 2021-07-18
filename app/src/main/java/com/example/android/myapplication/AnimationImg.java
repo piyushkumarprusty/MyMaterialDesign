da28ec20097e4f268fb7bc636fe32900318d05a6
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-18 19:11:07 +0530

    AnimationImg v1

--- app/src/main/java/com/example/android/myapplication/AnimationImg.java
+++ app/src/main/java/com/example/android/myapplication/AnimationImg.java
@@ -3,12 +3,28 @@ package com.example.android.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class AnimationImg extends AppCompatActivity {
+
+    ImageView img;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation_img);
+
+        img = findViewById(R.id.imageViewww);
+        btn = findViewById(R.id.btnanim);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
