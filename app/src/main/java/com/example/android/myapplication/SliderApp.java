0478c780657c3865bcf1200269fd4803be6e54d7
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-18 20:08:41 +0530

    SliderApp set back

--- app/src/main/java/com/example/android/myapplication/SliderApp.java
+++ app/src/main/java/com/example/android/myapplication/SliderApp.java
@@ -3,18 +3,29 @@ package com.example.android.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class SliderApp extends AppCompatActivity {
+
+    ViewFlipper flipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slider_app);
 
+        int imgarray[] = {R.drawable.pic17, R.drawable.pic7, R.drawable.pic6};
 
-
+        flipper = findViewById(R.id.flipper);
 
     }
+
+    public void showImage(int img){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(img);
+    }
+
 
 
 }
