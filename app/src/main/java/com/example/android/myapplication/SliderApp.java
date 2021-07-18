319d7e60d8ada9fba8f6c7347fb316b9ef676a41
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-18 19:56:42 +0530

    SliderApp v1

---
+++ app/src/main/java/com/example/android/myapplication/SliderApp.java
@@ -0,0 +1,14 @@
+package com.example.android.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SliderApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slider_app);
+    }
+}
