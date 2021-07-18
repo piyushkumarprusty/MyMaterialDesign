64e01c7fb237e8e163d741a92e4a48560a6ca24f
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-18 20:00:58 +0530

    SliderApp view flipper

--- app/src/main/java/com/example/android/myapplication/SliderApp.java
+++ app/src/main/java/com/example/android/myapplication/SliderApp.java
@@ -10,5 +10,11 @@ public class SliderApp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slider_app);
+
+
+
+
     }
+
+
 }
