7e4abda3a6465c7c0c13a5289fd8a0e9ec60bdcb
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-22 18:50:56 +0530

    CustomToasty layout2

--- app/src/main/java/com/example/android/myapplication/Taosty/CustomToasty.java
+++ app/src/main/java/com/example/android/myapplication/Taosty/CustomToasty.java
@@ -3,14 +3,27 @@ package com.example.android.myapplication.Taosty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.android.myapplication.R;
 
 public class CustomToasty extends AppCompatActivity {
 
+    Button btn1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_toasty);
+
+        btn1 =findViewById(R.id.button8);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
