ab92547b8d52fd966aa1c4827709bfa3ab86a3c1
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-22 18:43:24 +0530

    CustomToasty

---
+++ app/src/main/java/com/example/android/myapplication/Taosty/CustomToasty.java
@@ -0,0 +1,16 @@
+package com.example.android.myapplication.Taosty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android.myapplication.R;
+
+public class CustomToasty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_toasty);
+    }
+}
