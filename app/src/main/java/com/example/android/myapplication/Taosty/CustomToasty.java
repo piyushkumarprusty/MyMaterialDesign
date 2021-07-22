a6ff67a161629c8434e0b65540441a65b25bd3b4
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-22 19:12:14 +0530

    CustomToasty view

--- app/src/main/java/com/example/android/myapplication/Taosty/CustomToasty.java
+++ app/src/main/java/com/example/android/myapplication/Taosty/CustomToasty.java
@@ -3,8 +3,10 @@ package com.example.android.myapplication.Taosty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.android.myapplication.R;
 
@@ -22,6 +24,9 @@ public class CustomToasty extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                v = getLayoutInflater().inflate(R.layout.customtoast,findViewById(R.id.viewHolder));
+                TextView tv = v.findViewById(R.id.t1);
+                tv.setText("custom Toast");
             }
         });
 
