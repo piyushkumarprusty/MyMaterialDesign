6c3d5a224fb64c79f411da035bb1d86f9c969011
Piyush Kumar Prusty <piyushprusty10@gmail.com>
2021-07-22 21:21:43 +0530

    RecyclerView Add

---
+++ app/src/main/java/recyclerview/RecyclerView.java
@@ -0,0 +1,16 @@
+package recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android.myapplication.R;
+
+public class RecyclerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+    }
+}
